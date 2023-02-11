852e7df04136a52a460e1fa5f4355cbc957f5c27
Peekoe <peekoecode@gmail.com>
2023-02-11 14:40:15 -0800

    add error handling and movie index route

--- rest-api/main.py
+++ rest-api/main.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect
+
+trilogy = [
+  { "title": "Spider-Man",   "release-year": 2002, "director": "Sam Raimi" },
+  { "title": "Spider-Man 2", "release-year": 2004, "director": "Sam Raimi" },
+  { "title": "Spider-Man 3", "release-year": 2007, "director": "Sam Raimi" }
+]
 
 app = Flask(__name__)
 
@@ -8,10 +14,21 @@ def hello_world():
   <img src="https://upload.wikimedia.org/wikipedia/en/f/f3/Spider-Man2002Poster.jpg" />"""
 
 
-@app.route("/movies")
+@app.route("/spiderman-trilogy")
 def movies():
-  return [
-    { "title": "Spider-Man",   "release-year": 2002, "director": "Sam Raimi" },
-    { "title": "Spider-Man 2", "release-year": 2004, "director": "Sam Raimi" },
-    { "title": "Spider-Man 3", "release-year": 2007, "director": "Sam Raimi" }
-  ]
+  return trilogy
+
+
+@app.route("/spiderman-trilogy/<int:index>")
+def movie(index):
+  return trilogy[index - 1] if 0 < index < 4 else redirect('https://http.cat/404')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return redirect('https://http.cat/404')
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+  return redirect('https://http.cat/500')
